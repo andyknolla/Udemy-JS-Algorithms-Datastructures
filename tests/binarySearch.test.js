17448a2eb1e089b53b52c487cc497f9a8f68ac70
Andy <andyknolla@gmail.com>
2022-11-08 14:39:02 -0700

    Add Binary Search practice and tests

---
+++ tests/binarySearch.test.js
@@ -0,0 +1,50 @@
+/* binarySearch([1,2,3,4,5],2) // 1
+    binarySearch([1,2,3,4,5],3) // 2
+    binarySearch([1,2,3,4,5],5) // 4
+    binarySearch([1,2,3,4,5],6) // -1
+    binarySearch([
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+      40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+    ], 10) // 2
+    binarySearch([
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+      40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+    ], 95) // 16
+    binarySearch([
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+      40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+    ], 100) // -1
+    */
+
+const binarySearch = require('../binarySearch');
+
+test('Should find the val', () => {
+  expect(binarySearch([1,2,3,4,5],2)).toBe(1);
+});
+test('Should find the val', () => {
+  expect(binarySearch([1,2,3,4,5],3)).toBe(2);
+});
+test('Should find the val', () => {
+  expect(binarySearch([1,2,3,4,5],5)).toBe(4);
+});
+test('Should find the val', () => {
+  expect(binarySearch([1,2,3,4,5],6)).toBe(-1);
+});
+test('Should find the val', () => {
+  expect(binarySearch([
+    5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+    40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+  ], 10)).toBe(2);
+});
+test('Should find the val', () => {
+  expect(binarySearch([
+    5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+    40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+  ], 95)).toBe(16);
+});
+test('Should find the val', () => {
+  expect(binarySearch([
+    5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+    40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+  ], 100)).toBe(-1);
+});
