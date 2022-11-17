0738924a63271249877d4e95a284327cc8264b71
Andy <andyknolla@gmail.com>
2022-11-17 12:08:26 -0700

    Implement a singly-linked-list class with a few methods. Format sorting.js

---
+++ data_structures/singlyLinkedList.js
@@ -0,0 +1,63 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class singlyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (this.length === 0) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    newTail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  shift() {
+    if (!this.length) return undefined;
+    var prevHead = this.head;
+    this.head = this.head.next;
+    this.length--;
+    return prevHead;
+  }
+  unshift(val) {
+    var newNode = new Node(val);
+    if (!this.length) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
--- exercises/sorting.js
+++ exercises/sorting.js
@@ -6,41 +6,40 @@ const sorting = {
       for (var j = 0; j < i - 1; j++) {
         if (arr[j + 1] < arr[j]) {
           var temp = arr[j];
-          arr[j] = arr[j + 1]
+          arr[j] = arr[j + 1];
           arr[j + 1] = temp;
           swap = true;
         }
       }
-      if(!swap) break
+      if (!swap) break;
     }
     return arr;
   },
-  selectionSort: function(arr) {
-    for(var i = 0; i < arr.length; i++) {
+  selectionSort: function (arr) {
+    for (var i = 0; i < arr.length; i++) {
       var minIndex = i;
-      for(var j = i + 1; j < arr.length; j++) {
-      }
-      if(arr[j] < arr[minIndex]) minIndex = j;
-      if(i !== minIndex) {
+      for (var j = i + 1; j < arr.length; j++) {}
+      if (arr[j] < arr[minIndex]) minIndex = j;
+      if (i !== minIndex) {
         var temp = arr[i];
         arr[i] = arr[minIndex];
-        arr[minIndex] = temp
+        arr[minIndex] = temp;
       }
     }
     return arr;
   },
-  insertionSort: function(arr) {
-    for(var i = 1; i < arr.length; i++) {
-      for(var j = i - 1; j >= 0; j--) {
-        if(arr[i] < arr[j] && arr[i] > arr[j-1]) {
-          arr.splice(j, 0, arr.splice(arr[i],1));
+  insertionSort: function (arr) {
+    for (var i = 1; i < arr.length; i++) {
+      for (var j = i - 1; j >= 0; j--) {
+        if (arr[i] < arr[j] && arr[i] > arr[j - 1]) {
+          arr.splice(j, 0, arr.splice(arr[i], 1));
           break;
         }
       }
     }
     return arr;
   },
-  merge: function(arr1, arr2) {
+  merge: function (arr1, arr2) {
     // merge two sorted arrays
     var i = 0;
     var j = 0;
@@ -54,24 +53,50 @@ const sorting = {
         j++;
       }
     }
-    while(i < arr1.length) {
+    while (i < arr1.length) {
       merged.push(arr1[i]);
-      i++
+      i++;
     }
-    while(j < arr2.length) {
+    while (j < arr2.length) {
       merged.push(arr2[j]);
-      j++
+      j++;
     }
 
     return merged;
   },
-  mergeSort: function(arr) {
-    if(arr.length <= 1) return arr;
+  mergeSort: function (arr) {
+    if (arr.length <= 1) return arr;
     let mid = Math.floor(arr.length / 2);
-    let left = this.mergeSort(arr.slice(0,mid));
+    let left = this.mergeSort(arr.slice(0, mid));
     let right = this.mergeSort(arr.slice(mid));
     return this.merge(left, right);
+  },
+  swap: function (arr, pos1, pos2) {
+    var temp = arr[pos1];
+    arr[pos1] = arr[pos2];
+    arr[pos2] = temp;
+    return arr;
+  },
+  pivot: function (arr, start = 0, end = arr.at(-1)) {
+    var pivot = arr[start];
+    var pivotIndex = start; // starting pivot (arbitrary)
+    for (var i = start + 1; i < arr.length + 1; i++) {
+      if (arr[i] < pivot) {
+        pivotIndex++;
+        this.swap(arr, pivotIndex, i);
+      }
+    }
+    this.swap(arr, start,pivotIndex);
+    return pivotIndex;
+  },
+  quickSort: function (arr, left = 0, right = arr.length-1) {
+    if(left < right) {
+      var pivotIndex = this.pivot(arr, left, right);
+      this.quickSort(arr, left, pivotIndex - 1);
+      this.quickSort(arr, pivotIndex + 1, right);
+    }
+    return arr;
   }
-}
+};
 
 module.exports = sorting;
