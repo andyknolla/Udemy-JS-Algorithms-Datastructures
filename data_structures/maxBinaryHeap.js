5c21685f94b3cb1ea39157ddc3880ef063b878ba
Andy <andyknolla@gmail.com>
2022-12-13 20:28:03 -0700

    Implement Priority Queue class, include basic tests.

--- data_structures/maxBinaryHeap.js
+++ data_structures/maxBinaryHeap.js
@@ -8,35 +8,148 @@ class MaxBinaryHeap {
 
     if (this.values.length > 1) {
       // bubble up the new value
-      let newValIndex = this.values.length - 1;  // last item/ highest index
-      let parentIndex = Math.floor( (newValIndex - 1) / 2 ); 
-      while ( this.values[newValIndex] > this.values[parentIndex] ) {
+      let newValIndex = this.values.length - 1; // last item/ highest index
+      let parentIndex = Math.floor((newValIndex - 1) / 2);
+      while (this.values[newValIndex] > this.values[parentIndex]) {
         var temp = this.values[parentIndex];
         this.values[parentIndex] = this.values[newValIndex];
         this.values[newValIndex] = temp;
 
         newValIndex = parentIndex;
-        parentIndex =  Math.floor( (newValIndex - 1) / 2 ); 
-
+        parentIndex = Math.floor((newValIndex - 1) / 2);
       }
     }
     return this.values;
   }
 
+  heapify(arr) {
+    // create a new heap out of a given array of elements
+  }
+
   extractMax() {
-    const max = this.values.shift();  // [ 33, 39, 12, 27, 18 ]
-    this.values.unshift(this.values.pop());  // [ 18, 33, 39, 12, 27 ]
+    const max = this.values.shift(); // [ 33, 39, 12, 27, 18 ]
+    const end = this.values.pop();
 
-    // set parent
-    // set its left child
-    // set its right child
-    // compare
-
-
-
-
+    if (this.values.length > 0) {
+      this.values.unshift(end); // [ 18, 33, 39, 12, 27 ]
+      this.siftDown();
+    }
     return max;
+  }
+
+  siftDown() {
+    let parentIndex = 0; // last item/ highest index
+    const length = this.values.length;
+    const element = this.values[parentIndex];
+
+    while (true) {
+      let leftChildIndex = 2 * parentIndex + 1;
+      let rightChildIndex = 2 * parentIndex + 2;
+      var leftChildVal, rightChildVal;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChildVal = this.values[leftChildIndex];
+        if (leftChildVal > element) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChildVal = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChildVal > element) ||
+          (swap !== null && rightChildVal > leftChildVal)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+      if (swap === null) break;
+
+      this.values[parentIndex] = this.values[swap];
+      this.values[swap] = element;
+      parentIndex = swap;
+    }
   }
 }
 
-module.exports = MaxBinaryHeap;
+class Node {
+  constructor(priority, val) {
+    this.priority = priority;
+    this.val = val;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.values = []; // holds nodes
+  }
+
+  enqueue(priority, val) {
+    let newNode = new Node(priority, val);
+    this.values.push(newNode);
+    this.bubbleUp();
+    return this.values.map((node) => node.priority);
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIndex = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIndex];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIndex] = element;
+      this.values[idx] = parent;
+      idx = parentIndex;
+    }
+  }
+
+  deQueue() {
+    const max = this.values.shift(); // [ 33, 39, 12, 27, 18 ]
+    const end = this.values.pop();
+
+    if (this.values.length > 0) {
+      this.values.unshift(end); // [ 18, 33, 39, 12, 27 ]
+      this.siftDown();
+    }
+    return max;
+  }
+
+  siftDown() {
+    let parentIndex = 0; // last item/ highest index
+    const length = this.values.length;
+    const element = this.values[parentIndex];
+
+    while (true) {
+      let leftChildIndex = 2 * parentIndex + 1;
+      let rightChildIndex = 2 * parentIndex + 2;
+      var leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild.priority > element.priority) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild.priority > element.priority) ||
+          (swap !== null && rightChild.priority > leftChild.priority)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+      if (swap === null) break;
+
+      this.values[parentIndex] = this.values[swap];
+      this.values[swap] = element;
+      parentIndex = swap;
+    }
+  }
+}
+
+module.exports = { MaxBinaryHeap, PriorityQueue };
--- data_structures/tests/maxBinaryHeap.test.js
+++ data_structures/tests/maxBinaryHeap.test.js
@@ -1,65 +1,83 @@
-const MaxBinaryHeap = require('../maxBinaryHeap');
+const { MaxBinaryHeap, PriorityQueue, Node } = require('../maxBinaryHeap');
 
-test('Create an instance of MaxBinaryHeap', () => {
-  const heap = new MaxBinaryHeap();
-  expect(heap).toBeInstanceOf(MaxBinaryHeap);
-});
-
-test('Basic insertion; inserting 3 values in order should create an array in the same order as insertion', () => {
-  const heap = new MaxBinaryHeap();
-  heap.insert(41);
-  heap.insert(39);
-  heap.insert(33);
-
-  expect(heap.values).toEqual([41, 39, 33]);
-});
-
-test('Insertion of two values requiring reordering', () => {
-  const heap = new MaxBinaryHeap();
-  heap.insert(33);
-  heap.insert(41);
-  
-  expect(heap.values).toEqual([ 41, 33 ])
-});
-
-test('Insertion of 3 values requiring reordering', () => {
-  const heap = new MaxBinaryHeap();
-  heap.insert(39);
-  heap.insert(33);
-  heap.insert(41);
-  
-  expect(heap.values).toEqual([ 41, 33, 39 ])
-});
-
-test('Insertion of 6 values requiring reordering', () => {
-  const heap = new MaxBinaryHeap();
-  heap.insert(12);
-  heap.insert(27);
-  heap.insert(18);
-  heap.insert(33);
-  heap.insert(39);
-  heap.insert(41);
-  
-  expect(heap.values).toEqual([ 41, 33, 39, 12, 27, 18 ])
-});
-
-describe('Extract maximum from [ 41, 33, 39, 12, 27, 18 ]', () => {
-  const heap = new MaxBinaryHeap();
-  heap.insert(12);
-  heap.insert(27);
-  heap.insert(18);
-  heap.insert(33);
-  heap.insert(39);
-  heap.insert(41);
-  
-  test('Extracted value should be 41', () => {
-    expect(heap.extractMax()).toEqual(41);
+describe('Max Binary Heap', () => {
+  test('Create an instance of MaxBinaryHeap', () => {
+    const heap = new MaxBinaryHeap();
+    expect(heap).toBeInstanceOf(MaxBinaryHeap);
   });
-  test('Heap after initial swap should be [ 18, 33, 39, 12, 27 ]. (before sifting)', () => {
-    expect(heap.values).toEqual( [ 18, 33, 39, 12, 27 ] );
+
+  test('Basic insertion; inserting 3 values in order should create an array in the same order as insertion', () => {
+    const heap = new MaxBinaryHeap();
+    heap.insert(41);
+    heap.insert(39);
+    heap.insert(33);
+
+    expect(heap.values).toEqual([41, 39, 33]);
   });
-  xtest('Rearranged heap should be [39, 33, 18, 12, 27]', () => {
-    expect(heap.values).toEqual( [39, 33, 18, 12, 27] );
+
+  test('Insertion of two values requiring reordering', () => {
+    const heap = new MaxBinaryHeap();
+    heap.insert(33);
+    heap.insert(41);
+
+    expect(heap.values).toEqual([41, 33]);
+  });
+
+  test('Insertion of 3 values requiring reordering', () => {
+    const heap = new MaxBinaryHeap();
+    heap.insert(39);
+    heap.insert(33);
+    heap.insert(41);
+
+    expect(heap.values).toEqual([41, 33, 39]);
+  });
+
+  test('Insertion of 6 values requiring reordering', () => {
+    const heap = new MaxBinaryHeap();
+    heap.insert(12);
+    heap.insert(27);
+    heap.insert(18);
+    heap.insert(33);
+    heap.insert(39);
+    heap.insert(41);
+
+    expect(heap.values).toEqual([41, 33, 39, 12, 27, 18]);
+  });
+
+  describe('Extract maximum from [ 41, 33, 39, 12, 27, 18 ]', () => {
+    const heap = new MaxBinaryHeap();
+    heap.insert(12);
+    heap.insert(27);
+    heap.insert(18);
+    heap.insert(33);
+    heap.insert(39);
+    heap.insert(41);
+
+    test('Extracted value should be 41', () => {
+      expect(heap.extractMax()).toEqual(41);
+    });
+    xtest('Heap after initial swap should be [ 18, 33, 39, 12, 27 ]. (before sifting)', () => {
+      expect(heap.values).toEqual([18, 33, 39, 12, 27]);
+    });
+    test('Heapified heap should be [39, 33, 18, 12, 27]', () => {
+      expect(heap.values).toEqual([39, 33, 18, 12, 27]);
+    });
   });
 });
+//  TODO: finish this...
+describe('Priority Queue (Min Binary Heap with nodes)', () => {
+  test('Create an instance of MaxBinaryHeap', () => {
+    const queue = new PriorityQueue();
+    expect(queue).toBeInstanceOf(PriorityQueue);
+  });
 
+  test('Basic insertion; inserting 3 values in order should create an array in the same order as insertion', () => {
+    const queue = new PriorityQueue();
+    queue.enqueue(3, 41);
+    queue.enqueue(2, 39);
+    // queue.enqueue(1, 33);
+    const priorities = queue.values.map((node) => node.priority);
+    console.log('Priorities = ', priorities);
+    expect(priorities).toEqual([3, 2]);
+  });
+});
